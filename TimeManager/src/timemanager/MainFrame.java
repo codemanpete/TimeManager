169308189efe0cef2e1a8d07c64e402943639ed0
Brendan Walker <34696240+afbmwalker@users.noreply.github.com>
2019-01-25 14:41:16 -0600

    Add files via upload

---
+++ TimeManager/src/timemanager/MainFrame.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timemanager;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Brendan
+ */
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        super("Time Manager");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000,800);
+        setVisible(true);
+    }
+    
+}
---
+++ TimeManager/src/timemanager/TimeManager.java
@@ -0,0 +1,22 @@
+/*
+    CS 321
+    Team Project
+    Time Manager/Scheduler
+    Calendar
+    Business Meetings
+    
+Members:
+    Sean Curtis
+    Peter Cheng
+    Brendan Walker
+ */
+package timemanager;
+
+
+public class TimeManager {
+
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+    
+}
