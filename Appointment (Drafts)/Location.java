2e04ac2241c2eae0695a9967d71fb24036626fa5
seancurtis1983 <47042537+seancurtis1983@users.noreply.github.com>
2019-03-08 11:38:46 -0600

    2OB9OA: Draft the Location Class
    Location class, fully documented and tested

--- Appointment (Drafts)/Location.java
+++ Appointment (Drafts)/Location.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Location Class
+ * For CS321-02 Group Project
+ * Time Manager
  */
 package appointment;
 
 /**
  *
- * @author Sean
+ * @author Sean J Curtis
  */
 public class Location {
     private String address;
@@ -15,40 +15,70 @@ public class Location {
     private String latitude;
     private String longitude;
     
+    /**
+     * Default Constructor
+     */
     public Location() {
         
     }
     
+    /**
+     * Parameterized Constructor
+     * @param address  Sets location address
+     */
     public Location(String address) {
         this.address = address;
     }
-    
+    /**
+     * Parameterized Constructor, for lat long
+     * @param lat latitude
+     * @param lon longitude
+     */
     public Location(String lat, String lon) {
         this.latitude = lat;
         this.longitude = lon;
     }
-    
+    /**
+     * setAddress - sets address variable
+     * @param address address
+     */
     public void setAddress(String address) {
         this.address = address;
     }
-    
+    /**
+     * getAddress - returns address for this Location
+     * @return address
+     */
     public String getAddress() {
         return address;
     }
-    
+    /**
+     * setPhoneNumber - sets a phone number for this Location
+     * @param phoneNumber Phone number
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-    
+    /**
+     * getPhoneNumber - returns the phone number
+     * @return phoneNumber
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
-    
+    /** 
+     * setLatLong - sets latitude and longitude
+     * @param lat latitude
+     * @param lon longitude
+     */
     public void setLatLong(String lat, String lon) {
         this.latitude = lat;
         this.longitude = lon;
     }
-    
+    /**
+     * getLatLong - returns the lat and long for this Location
+     * @return s, String of the lat and long
+     */
     public String getLatLong() {
         String s = new String(latitude + " " + longitude);
         return s;
