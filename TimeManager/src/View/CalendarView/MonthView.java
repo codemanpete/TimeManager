41f30d14787858a623fa3d92f1f23105f256c172
Sean <Sean@Fluffy>
2019-04-06 16:49:11 -0500

    Added javadoc comments

--- TimeManager/src/View/CalendarView/MonthView.java
+++ TimeManager/src/View/CalendarView/MonthView.java
@@ -1,8 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+*    CS 321
+*    Team Project
+*    Time Manager/Scheduler
+*    Calendar
+*    Business Meetings
+*    
+*    MonthView - creates month calendar view
+*    
+*Members:
+*    Sean Curtis
+*    Peter Cheng
+*    Brendan Walker
+*    Charles McEniry
+*/
 package View.CalendarView;
 import java.awt.*;
 import javax.swing.*;
@@ -22,18 +32,27 @@ public class MonthView extends JPanel {
   
    
    
-    
+    /**
+     * Default Constructor
+     * @param model model object
+     */
     public MonthView(Model model) {
         month = today.get(Calendar.MONTH);
         this.model = model;
         
         this.add(makePanel());
     }
-    
+    /**
+     * Constructor with month variable, to change to a different month
+     * @param month int variable
+     */
     public MonthView(int month) {
         this.add(makePanel());
     }
-    
+    /**
+     * makePanel - creates the main panel
+     * @return JPanel of the calendar panel
+     */
     public JPanel makePanel() {
         
         
@@ -46,37 +65,34 @@ public class MonthView extends JPanel {
         panel.add(makeCalendarPanel(), BorderLayout.CENTER);
         return panel;
     }
-    
+    /**
+     * makeTopPanel - creates the label panel
+     * @return a JPanel of the label
+     */
     private JPanel makeTopPanel() {
         
-       // String[] MONTHNAMES = { "January", "February", "March", "April", 
-       //                        "May", "June", "July", "August",
-       //                        "September", "October", "November", "December" };
-        //today = Calendar.getInstance();
+       
         today.set(Calendar.MONTH, (month));
         JPanel panel = new JPanel(true);
         panel.setBorder(BorderFactory.createBevelBorder(1));
         panel.setLayout(new FlowLayout());
         panel.setBackground(Color.white);
-        //JLabel label = new JLabel(MONTHNAMES[month]);
+        
         JLabel label = new JLabel(model.getMonthName() + " " + model.getYear());
         label.setForeground(Color.red);
         panel.add(label, BorderLayout.CENTER);
         
         return panel;
     }
-    
+    /**
+     * makeCalendarPanel - creates the actual calendar panel
+     * @return a JPanel for the Calendar
+     */
     private JPanel makeCalendarPanel() {
         JPanel panel = new JPanel(true);
         panel.setBorder(BorderFactory.createLineBorder(Color.black));
         panel.setLayout(new GridLayout(0, 7));
-       // today = Calendar.getInstance();
-        //Calendar firstDay = Calendar.getInstance();
-        //firstDay = today;
-        //firstDay.set(Calendar.DAY_OF_MONTH, (today.get(Calendar.DAY_OF_MONTH)-(Calendar.DAY_OF_MONTH - 1)));
-       // firstDay.set(Calendar.DAY_OF_MONTH, 1);
-        
-        //int td = today.get(Calendar.DAY_OF_MONTH);
+       
         
         
         if (model.getCurrent().get(Calendar.YEAR) == model.getYear() && 
@@ -88,15 +104,7 @@ public class MonthView extends JPanel {
         }
         
         
-        //System.out.println(td);
-      
         
-        //String[] WEEKDAYS = {"Sunday", "Monday", "Tuesday", "Wednesday",
-         //                    "Thursday", "Friday", "Saturday"};
-        
-        //System.out.println(WEEKDAYS[firstDay.get(Calendar.DAY_OF_WEEK)]);
-        //System.out.println(WEEKDAYS[firstDay.get(Calendar.DAY_OF_WEEK) - 1]);
-        //System.out.println(today.get(Calendar.DAY_OF_WEEK));
        
         for (int i = 0; i < 7; i++) {
             JPanel panels = new JPanel(true);
@@ -110,15 +118,12 @@ public class MonthView extends JPanel {
             panel.add(panels);
         }
         
-        //int[] DAYS_IN_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
-        
-        //for (int i = 1; i <= (DAYS_IN_MONTH[today.get(Calendar.MONTH)] + firstDay.get(Calendar.DAY_OF_WEEK) - 1); i++) {
         for (int i = 1; i <= (model.getDaysInMonth() + model.getFirstDay() - 1); i++) {
             
             
             
-            //if (i < (firstDay.get(Calendar.DAY_OF_WEEK )  )) {
+         
             if (i < (model.getFirstDay()  )) {
                 JPanel panels = new JPanel(true);
                 panels.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -126,14 +131,11 @@ public class MonthView extends JPanel {
             }
             
             else {
-           //String startday = new String()
-            
-            
-            //JLabel labels = new JLabel(" " +(i - (firstDay.get(Calendar.DAY_OF_WEEK) - 1)));
+           
             JLabel labels = new JLabel((i - (model.getFirstDay() - 1))+ " ");
             labels.setAlignmentY(Component.LEFT_ALIGNMENT);
             
-            //if ((i - (firstDay.get(Calendar.DAY_OF_WEEK) - 1)) == td) {
+            
             if ((i - (model.getFirstDay() - 1)) == td) {
                 labels.setForeground(Color.red);
             }
@@ -141,22 +143,27 @@ public class MonthView extends JPanel {
             NewDayPanel panels = new NewDayPanel(labels, model);
             panels.setBorder(BorderFactory.createLineBorder(Color.black));
             panels.setPreferredSize(new Dimension(175, 125));
-            //panels.add(labels, BorderLayout.NORTH);
+           
             panel.add(panels);
             }
         }
         return panel;
     }
-    
+    /**
+     * getMonth - returns current month variable
+     * @return int for the current month
+     */
     public int getMonth() {
         return month;
     }
-    
+    /**
+     * addMonth - increments month variable (Deprecated)
+     */
     public void addMonth() {
         int td = today.get(Calendar.MONTH);
         td++;
         today.set(Calendar.MONTH, td);
-        System.out.println(today.get(Calendar.MONTH));
+        //System.out.println(today.get(Calendar.MONTH));
         
        
     }
