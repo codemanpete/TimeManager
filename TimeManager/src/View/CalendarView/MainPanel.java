536bc6470a901137141490ccefc8ba5b56d10d7d
Sean <Sean@Fluffy>
2019-04-06 16:40:06 -0500

    Added javadoc comments

--- TimeManager/src/View/CalendarView/MainPanel.java
+++ TimeManager/src/View/CalendarView/MainPanel.java
@@ -1,8 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+*    CS 321
+*    Team Project
+*    Time Manager/Scheduler
+*    Calendar
+*    Business Meetings
+*    
+*    MainPanel class - Creates a panel in main frame to display the calendar
+*    
+*Members:
+*    Sean Curtis
+*    Peter Cheng
+*    Brendan Walker
+*    Charles McEniry
+*/
 package View.CalendarView;
 import java.awt.*;
 import javax.swing.*;
@@ -20,7 +30,11 @@ public class MainPanel extends JPanel {
     
     
     
-    
+    /**
+     * Default Constructor
+     * @param model model object
+     * @param user User object
+     */
     public MainPanel(Model model, User user) {
         //setPreferredSize(new Dimension(1500, 1500));
         setLayout(new CardLayout());
@@ -34,7 +48,9 @@ public class MainPanel extends JPanel {
         add(calPanel);
         
     }
-    
+    /**
+     * changeWeek - changes to week view
+     */
     public void changeWeek() {
         removeAll();
         this.calPanel = new WeekView(model);
@@ -44,7 +60,9 @@ public class MainPanel extends JPanel {
         
         
     }
-    
+    /**
+     * changeMonth - changes to month view
+     */
     public void changeMonth() {
         removeAll();
         this.calPanel = new MonthView(model);
@@ -53,7 +71,9 @@ public class MainPanel extends JPanel {
         repaint();
         
     }
-    
+    /**
+     * nextMonth - advances calendar
+     */
     public void nextMonth() {
         
       removeAll();
@@ -61,7 +81,9 @@ public class MainPanel extends JPanel {
       revalidate();
       repaint();
     }
-    
+    /**
+     * paintComponent - repaints the view
+     */
     public void paintComponent() {
         removeAll();
         add(calPanel = new MonthView(model));
