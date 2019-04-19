2cd3cf422da72fbbc532f7cc47f98f37e82436df
Sean <Sean@Fluffy>
2019-04-19 15:53:40 -0500

    Added Simple Contact List

--- TimeManager/src/View/CalendarView/MainFrame.java
+++ TimeManager/src/View/CalendarView/MainFrame.java
@@ -22,6 +22,7 @@ import javax.swing.*;
 import View.Controller.*;
 import User.*;
 import View.CalendarDialog.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -44,31 +45,76 @@ public class MainFrame extends JFrame {
         control = new CalController(main, model);
         control.setBorder(BorderFactory.createBevelBorder(1));
         JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("File");
+        JMenu menu1 = new JMenu("File");
+        JMenu menu2 = new JMenu("Contact");
+        JMenuItem global = new JMenuItem("Global");
         JMenuItem addAppt = new JMenuItem("Add Appointment");
         JMenuItem addUser = new JMenuItem("Add User");
+        JMenuItem exitProgram = new JMenuItem("Exit");
         addAppt.addActionListener(new ActionListener() { 
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Add Appointment");
+                //System.out.println("Add Appointment");
                 AddAppointmentDialog apptDialog = new AddAppointmentDialog(frame, true, user, main);
                 apptDialog.setLocationRelativeTo(frame);
                 apptDialog.setVisible(true);
             }
         });
+        
         addUser.addActionListener(new ActionListener() { 
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Add user");
+                //System.out.println("Add user");
                 AddUserDialog userDialog = new AddUserDialog(frame, true);
                 userDialog.setLocationRelativeTo(frame);
                 userDialog.setVisible(true);
             }
         });
-        menu.add(addAppt);
-        menu.addSeparator();
-        menu.add(addUser);
-        menuBar.add(menu);
+        
+        exitProgram.addActionListener(new ActionListener() { 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               // System.out.println("Add user");
+                System.exit(0);
+            }
+        });
+        
+        global.addActionListener(new ActionListener() { 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               UserDB udb = new UserDB("jdbc:sqlite:user.db");
+               ArrayList<User> users = udb.getAllUsers();
+               JFrame contact = new JFrame("Contact List");
+               contact.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+               contact.setSize(200, 200);
+               contact.setLayout(new BorderLayout());
+               JPanel panel = new JPanel(true);
+               panel.setLayout(new GridLayout(0, 1));
+               
+               for (User a : users) {
+                   JPanel user = new JPanel();
+                   JLabel label1 = new JLabel();
+                   JLabel label2 = new JLabel();
+                   label1.setText(" " + a.getIdNumber());
+                   label2.setText(a.getUserName());
+                   user.add(label1);
+                   user.add(label2);
+                   panel.add(user);
+               }
+               contact.add(panel);
+               contact.setVisible(true);
+               
+            }
+        });
+        
+        menu1.add(addAppt);
+        menu1.addSeparator();
+        menu1.add(addUser);
+        menu1.addSeparator();
+        menu1.add(exitProgram);
+        menu2.add(global);
+        menuBar.add(menu1);
+        menuBar.add(menu2);
         frame.setTitle("Time Manager");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
